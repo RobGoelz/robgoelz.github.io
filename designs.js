b1da1a94c9c99c5f414f5126dafee8f44706b24f
Rob Goelz <RobGoelz@gmail.com>
2018-04-05 10:31:38 -0400

    updating site with PAM project from GWG

---
+++ designs.js
@@ -0,0 +1,42 @@
+const addRow = function () {
+  $('table').append("<tr></tr>");
+}
+
+const addCol = function () {
+  $( 'tr:last' ).append("<td></td>");
+      $('td').attr('class','pixel');
+});
+
+const clearCanvas = function () {
+   $('table tr').remove();
+}
+
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function makeGrid(event) {
+  clearCanvas();
+
+  const rows=$('#inputHeight').val();
+  const cols=$('#inputWeight').val();
+
+  for (let i = 1; i <= rows; i++) {
+    // $('table').append("<tr></tr>");
+    addRow();
+    for (let j = 1; j <= cols; j++) {
+      // $( 'tr:last' ).append("<td></td>");
+      // $('td').attr('class','pixel');
+      addCol();
+    }
+  }
+    event.preventDefault();
+});
+
+// Select color input
+$("#pixelCanvas").on('click','.pixel',function(){
+  let paint=$("#colorPicker").val();
+  $( this ).css("background-color",paint);
+});
+
+// $("#pixelCanvas").drag('.pixel',function(event){
+//   let paint=$("#colorPicker").val();
+//   $( this ).css("background-color",paint);
+// });
